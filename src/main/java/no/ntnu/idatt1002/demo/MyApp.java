baada30504058e29c701e9a50806fd6aaa6123f1
Jacob Lein <jacoble@stud.ntnu.no>
2024-01-09 10:53:39 +0100

    Jacob test

--- src/main/java/no/ntnu/idatt1002/demo/MyApp.java
+++ src/main/java/no/ntnu/idatt1002/demo/MyApp.java
@@ -15,5 +15,6 @@ public class MyApp {
         MyWindow window = new MyWindow("The Window");
         window.setVisible(true);
         System.out.println("Helloe");
+        System.out.println("Hello world!");
    }  
 }
