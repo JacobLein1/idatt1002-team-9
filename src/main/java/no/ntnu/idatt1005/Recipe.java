967c55b704c4d4fc608eb1cc66aa7ac30f1a730e
Therese Synnøve Rondeel <thereron@stud.ntnu.no>
2024-02-27 11:54:40 +0100

    Implement Recipe and RecipeList methods

--- src/main/java/no/ntnu/idatt1005/Recipe.java
+++ src/main/java/no/ntnu/idatt1005/Recipe.java
@@ -1,4 +1,28 @@
 package no.ntnu.idatt1005;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Recipe {
+  private final Map<String, Integer> ingredients;
+  private final String instructions;
+  private final int numberOfPeople;
+
+  public Recipe(Map<String, Integer> ingredients, String instructions, int numberOfPeople) {
+    this.ingredients = ingredients;
+    this.instructions = instructions;
+    this.numberOfPeople = numberOfPeople;
+  }
+
+  public Map<String, Integer> getIngredients() {
+    return ingredients;
+  }
+
+  public String getInstructions() {
+    return instructions;
+  }
+
+  public int getNumberOfPeople() {
+    return numberOfPeople;
+  }
 }
