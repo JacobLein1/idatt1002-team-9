09cadf2fb8bb4b3f9b927880469113c40e95d72b
sigridhoel <sigrho@ntnu.stud.no>
2024-02-27 13:30:28 +0100

    revert methods

--- src/main/java/no/ntnu/idatt1005/GroceryList.java
+++ src/main/java/no/ntnu/idatt1005/GroceryList.java
@@ -1,24 +1,4 @@
 package no.ntnu.idatt1005;
 
-import java.util.ArrayList;
-
 public class GroceryList {
-    private ArrayList<Grocery> groceryList;
-
-    public GroceryList(){
-        this.groceryList = new ArrayList<>();
-    }
-
-    public void addGrocery(Grocery grocery){
-        groceryList.add(grocery);
-    }
-
-    public void removeGrocery(Grocery grocery){
-        groceryList.remove(grocery);
-    }
-
 }
-
-
-
-
