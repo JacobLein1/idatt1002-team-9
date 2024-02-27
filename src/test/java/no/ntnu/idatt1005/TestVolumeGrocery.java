5573e7026aafd37dbd922efb6e8562f7eac3195a
Jacob Lein <jacoble@stud.ntnu.no>
2024-02-27 10:42:37 +0100

    Fix TestVolumeGrocery

--- src/test/java/no/ntnu/idatt1005/TestVolumeGrocery.java
+++ src/test/java/no/ntnu/idatt1005/TestVolumeGrocery.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 public class TestVolumeGrocery {
     @Test
     void testVolumeGrocery() {
-        VolumeGrocery vg = new VolumeGrocery("Melk", "bilde", 1, "liter");
+        VolumeGrocery vg = new VolumeGrocery("Melk", "bilde", 1, "l");
         Assertions.assertEquals(vg.getName(),"Melk");
         Assertions.assertEquals(vg.getImage(),"bilde");
         Assertions.assertEquals(vg.getAmount(), 1);
-        Assertions.assertEquals(vg.getUnit(3), "liter");
+        Assertions.assertEquals(vg.getUnit(3), "l");
 
     }
 }
