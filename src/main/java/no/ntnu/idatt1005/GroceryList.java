a7b81d858944192f7b9c71fb92ceb5fe8cd9b131
sigridhoel <sigrho@ntnu.stud.no>
2024-02-27 13:19:57 +0100

    add methods for GroceryList

--- src/main/java/no/ntnu/idatt1005/GroceryList.java
+++ src/main/java/no/ntnu/idatt1005/GroceryList.java
@@ -1,4 +1,24 @@
 package no.ntnu.idatt1005;
 
+import java.util.ArrayList;
+
 public class GroceryList {
+    private ArrayList<Grocery> groceryList;
+
+    public GroceryList(){
+        this.groceryList = new ArrayList<>();
+    }
+
+    public void addGrocery(Grocery grocery){
+        groceryList.add(grocery);
+    }
+
+    public void removeGrocery(Grocery grocery){
+        groceryList.remove(grocery);
+    }
+
 }
+
+
+
+
