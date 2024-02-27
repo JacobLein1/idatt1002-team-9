80c124a2910451667bc86cb8200645c60023cf6f
Therese Synnøve Rondeel <thereron@stud.ntnu.no>
2024-02-27 13:49:26 +0100

    Implement test-methods

--- src/test/java/no/ntnu/idatt1005/TestRecipe.java
+++ src/test/java/no/ntnu/idatt1005/TestRecipe.java
@@ -1,4 +1,26 @@
 package no.ntnu.idatt1005;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestRecipe {
+
+  @Test
+  @DisplayName("Test constructor with valid input")
+  void testConstructorWithValidInput() {
+    Map<String, Integer> ingredients = new HashMap<>();
+    ingredients.put("1", 400);
+    ingredients.put("4", 100);
+    String instructions = "Add the chicken nuggets to the pasta";
+    int numberOfPeople = 4;
+
+    Recipe recipe = new Recipe(ingredients, instructions, numberOfPeople);
+    assertEquals(ingredients, recipe.getIngredients());
+    assertEquals(instructions, recipe.getInstructions());
+    assertEquals(numberOfPeople, recipe.getNumberOfPeople());
+  }
 }
--- src/test/java/no/ntnu/idatt1005/TestRecipeList.java
+++ src/test/java/no/ntnu/idatt1005/TestRecipeList.java
@@ -1,4 +1,52 @@
 package no.ntnu.idatt1005;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class TestRecipeList {
+
+  @Test
+  @DisplayName("Test constructor initialization works")
+  void testConstructorInitialization() {
+    RecipeList recipeList = new RecipeList();
+    assertNotNull(recipeList.getRecipeList());
+  }
+
+  @Test
+  @DisplayName("Test addRecipe works with valid input")
+  void testAddRecipeWorksWithValidInput() {
+    RecipeList recipeList = new RecipeList();
+
+    Map<String, Integer> ingredients = new HashMap<>();
+    ingredients.put("1", 400);
+    ingredients.put("4", 100);
+    String instructions = "Add the chicken nuggets to the pasta";
+    int numberOfPeople = 4;
+    String recipeId = "1";
+
+    assertDoesNotThrow(() ->
+        recipeList.addRecipe(recipeId, ingredients, instructions, numberOfPeople));
+  }
+
+  @Test
+  @DisplayName("Test removeRecipe works with valid input")
+  void testRemoveRecipeWorksWithValidInput() {
+    RecipeList recipeList = new RecipeList();
+
+    Map<String, Integer> ingredients = new HashMap<>();
+    ingredients.put("1", 400);
+    ingredients.put("4", 100);
+    String instructions = "Add the chicken nuggets to the pasta";
+    int numberOfPeople = 4;
+    String recipeId = "1";
+
+    recipeList.addRecipe(recipeId, ingredients, instructions, numberOfPeople);
+
+    assertDoesNotThrow(() ->
+        recipeList.removeRecipe(recipeId));
+  }
 }
