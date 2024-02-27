bacfb50c370abee2fbfa0d0a2a5d7872834cc914
Mikael Stray Frøyshov <mikaelsf@stud.ntnu.no>
2024-02-27 12:20:29 +0100

    feat: create unit classes

--- src/main/java/no/ntnu/idatt1005/Grocery/Grocery.java
+++ src/main/java/no/ntnu/idatt1005/Grocery/Grocery.java
@@ -1,16 +1,21 @@
-package no.ntnu.idatt1005;
+package no.ntnu.idatt1005.Grocery;
+
+import no.ntnu.idatt1005.Unit.Unit;
 
 public class Grocery {
     private String name;
     private String image;
     private float amount;
+    private final String id;
+    private Unit unit;
 
-    Grocery(){}
 
-    Grocery(String name, String image, float amount) {
+    Grocery(String name, String image, float amount, String id, Unit unit) {
         this.name = name;
         this.image = image;
         this.amount = amount;
+        this.id = id;
+        this.unit = unit;
     }
 
     public String getName() {
@@ -23,6 +28,15 @@ public class Grocery {
     public float getAmount() {
         return amount;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
     public float setAmount(float input) {
         return amount;
     }
---
+++ src/main/java/no/ntnu/idatt1005/Unit/VolumeUnit.java
@@ -0,0 +1,9 @@
+package no.ntnu.idatt1005.Unit;
+
+public class VolumeUnit extends Unit {
+
+    public VolumeUnit(String name) {
+        super(name);
+    }
+
+}
