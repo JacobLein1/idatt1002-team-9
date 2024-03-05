e1c1cb4c2c977d57980f940a6e64a13560ce94a0
Therese Synnøve Rondeel <thereron@stud.ntnu.no>
2024-03-05 09:35:00 +0100

    Implement exception handling

--- src/main/java/no/ntnu/idatt1005/RecipeList.java
+++ src/main/java/no/ntnu/idatt1005/RecipeList.java
@@ -13,12 +13,19 @@ public class RecipeList {
     return recipeList;
   }
 
-  public void addRecipe(String id, Map<String, Integer> ingredients, String instructions, int numberOfPeople) {
+  public void addRecipe(String id, Map<String, Integer> ingredients, String instructions,
+                        int numberOfPeople) throws IllegalArgumentException {
+    if (recipeList.containsKey(id)) {
+      throw new IllegalArgumentException("There already exists a recipe with the requested id");
+    }
     Recipe recipe = new Recipe(ingredients, instructions, numberOfPeople);
     recipeList.put(id, recipe);
   }
 
   public void removeRecipe(String id) {
+    if (!recipeList.containsKey(id)) {
+      throw new IllegalArgumentException("There doesn't exist a recipe with the existing id");
+    }
     recipeList.remove(id);
   }
 
