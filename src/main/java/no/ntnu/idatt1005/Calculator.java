87429a8d292e83dc262b50d56f686ad015dcd6b6
Mikael Stray Frøyshov <mikaelsf@stud.ntnu.no>
2024-03-05 13:21:01 +0100

    feat(calculator): calculator class update

--- src/main/java/no/ntnu/idatt1005/Calculator.java
+++ src/main/java/no/ntnu/idatt1005/Calculator.java
@@ -1,6 +1,20 @@
 package no.ntnu.idatt1005;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Calculator {
 
+    private Basket basket;
+    private Inventory inventory;
+
+    public Calculator(Basket basket, Inventory inventory) {
+        this.basket = basket;
+        this.inventory = inventory;
+    }
+
+    public HashMap<String, Double> neededGroceries(Recipe recipe, Inventory inventory) {
+        for (Map.Entry)
+    }
 
 }
