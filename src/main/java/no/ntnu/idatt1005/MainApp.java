7899f1b71e77b7cdfba9121c4902dfb08c6ad4f4
Mikael Stray Frøyshov <mikaelsf@stud.ntnu.no>
2024-03-11 13:35:36 +0100

    feat(gui): implement gui main class

---
+++ src/main/java/no/ntnu/idatt1005/MainApp.java
@@ -0,0 +1,16 @@
+package no.ntnu.idatt1005;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
