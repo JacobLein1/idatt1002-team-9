4cf833d5b78cc616a98c20b80180f6f8143ff899
sigridhoel <sigrho@ntnu.stud.no>
2024-03-15 11:55:41 +0100

    Implemented methods for GroceryDAO

--- src/main/java/no/ntnu/idatt1005/dao/GroceryDAO.java
+++ src/main/java/no/ntnu/idatt1005/dao/GroceryDAO.java
@@ -1,8 +1,14 @@
 package no.ntnu.idatt1005.dao;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import no.ntnu.idatt1005.Grocery.Grocery;
 import no.ntnu.idatt1005.Grocery.GroceryList;
+import no.ntnu.idatt1005.Unit.UnitsE;
+
+import static no.ntnu.idatt1005.dao.DBConnectionProvider.close;
 
 public class GroceryDAO {
   private DBConnectionProvider connectionProvider;
@@ -12,7 +18,7 @@ public class GroceryDAO {
   }
 
 
-  /*public Grocery getGroceryById(int id) {
+  public Grocery getGroceryById(int id) {
     Connection connection = null;
     PreparedStatement preparedStatement = null;
     ResultSet resultSet = null;
@@ -23,25 +29,84 @@ public class GroceryDAO {
       preparedStatement = connection.prepareStatement("SELECT * FROM GroceryRegister WHERE groceryId=?");
       preparedStatement.setInt(1, id);
       resultSet = preparedStatement.executeQuery();
-      user = getUserFromResultSet(resultSet);
+      grocery = getGroceryFromResultSet(resultSet);
     } catch (SQLException e) {
       e.printStackTrace();
     }finally{
       close(connection, preparedStatement, resultSet);
     }
-    return user;
-
-    return null;
-  }*/
-
-  public GroceryList getAllGroceries() {
-    return null;
+    return grocery;
   }
 
-  public void addGrocery() {}
+  private Grocery getGroceryFromResultSet(ResultSet resultSet){
+    Grocery grocery = null;
+    try{
+        if(resultSet.next()){
+          String name = resultSet.getString("name");
+          String image = resultSet.getString("image");
+          String groceryId = String.valueOf(resultSet.getInt("groceryId"));
+          String unitString = resultSet.getString("unit");
+          UnitsE unit = UnitsE.valueOf(unitString.toLowerCase());
+          try{
+            grocery = new Grocery (name, image, groceryId, unit);
+          } catch (Exception e){
+            e.printStackTrace();
+          }
+        }
 
-  public void removeGrocery() {
-    //kan være denne ikke er nødvendig
+        } catch (SQLException e) {
+        e.printStackTrace();
+    } finally {
+      close(null, null, resultSet);
+    }
+    return grocery;
+  }
+
+  public List<Grocery> getAllGroceries() {
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    List<Grocery> groceryList = new ArrayList<Grocery>();
+
+    try {
+      connection = connectionProvider.getConnection();
+      preparedStatement = connection.prepareStatement("SELECT * FROM GroceryRegister");
+      resultSet = preparedStatement.executeQuery();
+
+      while(resultSet.next()){
+        Grocery grocery = getGroceryFromResultSet(resultSet);
+        groceryList.add(grocery);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }finally{
+      close(connection, preparedStatement, resultSet);
+    }
+    return groceryList;
+  }
+
+  public void addGrocery(String name, String image, UnitsE unit) throws IllegalArgumentException {
+
+    if (name == null || image == null || unit == null) {
+      throw new IllegalArgumentException("Name, image and unit cannot be null");
+    }
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+
+    try {
+        connection = connectionProvider.getConnection();
+        preparedStatement = connection.prepareStatement("INSERT INTO GroceryRegister (name, image, unit) VALUES (?, ?, ?)");
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, image);
+        preparedStatement.setString(3, unit.getUnit());
+        preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+        e.printStackTrace();
+    }finally{
+      close(connection, preparedStatement, resultSet);
+    }
   }
 
 }
