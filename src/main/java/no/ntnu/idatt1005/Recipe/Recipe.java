fb1374867986a91da03787fce2c58f10cb44c88b
sigridhoel <sigrho@ntnu.stud.no>
2024-03-15 11:55:50 +0100

    Add fields

--- src/main/java/no/ntnu/idatt1005/Recipe/Recipe.java
+++ src/main/java/no/ntnu/idatt1005/Recipe/Recipe.java
@@ -4,14 +4,19 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class Recipe {
+  private final String recipeID;
+
+  private final String recipeName;
   private final Map<String, Double> ingredients;
   private final String instructions;
   private final int numberOfPeople;
 
-  public Recipe(Map<String, Double> ingredients, String instructions, int numberOfPeople) {
+  public Recipe(String recipeID, String recipeName, String instructions, int numberOfPeople, Map<String, Double> ingredients) {
     this.ingredients = ingredients;
     this.instructions = instructions;
     this.numberOfPeople = numberOfPeople;
+    this.recipeID = recipeID;
+    this.recipeName = recipeName;
   }
 
   public Map<String, Double> getIngredients() {
