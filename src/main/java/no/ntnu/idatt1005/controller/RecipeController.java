0ac30e7e048e2f3aed19e976b450660007fefa99
Therese Synnøve Rondeel <thereron@stud.ntnu.no>
2024-03-18 14:20:58 +0100

    Implement methods for RecipeController

---
+++ src/main/java/no/ntnu/idatt1005/controller/RecipeController.java
@@ -0,0 +1,19 @@
+package no.ntnu.idatt1005.controller;
+
+import no.ntnu.idatt1005.Recipe.Recipe;
+import no.ntnu.idatt1005.dao.DBConnectionProvider;
+import no.ntnu.idatt1005.dao.RecipeDAO;
+import java.util.List;
+import java.util.List.*;
+
+public class RecipeController {
+  RecipeDAO recipeDAO;
+
+  public RecipeController() {
+    recipeDAO = new RecipeDAO(DBConnectionProvider.instance());
+  }
+
+  public List<Recipe> getAllRecipes() {
+    return recipeDAO.getRecipes();
+  }
+}
