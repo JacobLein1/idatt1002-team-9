13fb272b80e384b78ed8c38def369c2f92b3798e
Jacob Lein <jacoble@stud.ntnu.no>
2024-03-20 13:08:18 +0100

    Start creation of shoppingCartContent

--- src/main/java/no/ntnu/idatt1005/view/MainApp.java
+++ src/main/java/no/ntnu/idatt1005/view/MainApp.java
@@ -1,10 +1,12 @@
-package no.ntnu.idatt1005;
+package no.ntnu.idatt1005.view;
 
 import javafx.application.Application;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
@@ -14,10 +16,14 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import no.ntnu.idatt1005.controller.BasketController;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainApp extends Application {
 
@@ -81,7 +87,7 @@ public class MainApp extends Application {
             tabRecipe.setContent(recipeContent);
 
             // Set content for Shopping Cart tab (Placeholder for actual content)
-            VBox shoppingCartContent = new VBox(new Label("Shopping Cart Content"));
+            VBox shoppingCartContent = new VBox();
             tabShoppingCart.setContent(shoppingCartContent);
             Text shoppingCartText = new Text("Shopping Cart");
             shoppingCartText.setFont(titleFont);
@@ -90,26 +96,60 @@ public class MainApp extends Application {
             shoppingCartContent.getChildren().addAll(shoppingCartText, shoppingCartDescription);
 
 
+            // Create a recipe box with text and counter
             VBox yoghurtBox = new VBox();
             Text yoghurtWFruits = new Text("Yoghurt with fruits");
-
             IntegerProperty yoghurtAmount = new SimpleIntegerProperty(0);
             Text counterText = new Text();
             counterText.textProperty().bind(yoghurtAmount.asString());
 
+            //Create buttons to increase and decrease the amount of yoghurt
             Button increaseYogButton = new Button("+");
-
             increaseYogButton.setOnAction(e -> yoghurtAmount.set(yoghurtAmount.get() + 1));
-
             Button decreaseYogButton = new Button("-");
-
             decreaseYogButton.setOnAction( e -> {
                 if (yoghurtAmount.get() > 0) {
                     yoghurtAmount.set(yoghurtAmount.get() - 1);
                 }
             });
 
-            yoghurtBox.getChildren().addAll(yoghurtWFruits, increaseYogButton, counterText, decreaseYogButton);
+            //Add buttons to a HBox
+            HBox yogButtonBox = new HBox(decreaseYogButton, counterText, increaseYogButton);
+            yogButtonBox.setSpacing(10);
+            yoghurtBox.setPadding(new Insets(30));
+
+            //Add text and buttons to the yoghurtBox
+            yoghurtBox.getChildren().addAll(yoghurtWFruits, yogButtonBox);
+            shoppingCartContent.getChildren().add(yoghurtBox);
+
+            //Create HBox for the result button
+            HBox resultButtonBox = new HBox();
+            resultButtonBox.setAlignment(Pos.BOTTOM_RIGHT);
+            Button resultButton = new Button("Finish shopping ->");
+            resultButtonBox.getChildren().add(resultButton);
+            shoppingCartContent.getChildren().add(resultButtonBox);
+
+
+            //Node originalContent = tabShoppingCart.getContent();
+            //Create a new scene for the shopping cart
+            resultButton.setOnAction(e ->{
+                tabShoppingCart.setContent(null);
+                Scene shoppingCartScene = new Scene(tabPane);
+                Text shoppingListText = new Text("Shopping List");
+                shoppingCartContent.getChildren().addAll(shoppingCartText, shoppingListText);
+
+
+                BasketController basketController = new BasketController();
+
+                //Get the shopping list from the basket as an observable list
+                List<String[]> list = basketController.getShoppingListFromBasket();
+                ObservableList<String> shoppingList = convertListToObservableList(list);
+                ListView<String> shoppingListView = new ListView<>(shoppingList);
+                shoppingCartContent.getChildren().add(shoppingListView);
+
+                primaryStage.setScene(shoppingCartScene);
+
+            });
 
 
 
@@ -140,6 +180,14 @@ public class MainApp extends Application {
             primaryStage.show();
     }
 
+    /*@Param - list: List<String[]>
+    * @return - ObservableList<String>
+    * This method takes a list of String arrays and converts it to an ObservableList
+    * */
+    public ObservableList<String> convertListToObservableList(List<String[]> list) {
+    return list.stream().flatMap(Arrays::stream)
+                .collect(Collectors.toCollection(FXCollections::observableArrayList));
+    }
     @Override
     public void init() throws Exception {
         super.init();
