f1c2465886482b05e7668da5ddd4488dde986201
sanderrb <sanderrb@stud.ntnu.no>
2024-03-25 14:06:08 +0100

    addGrocery works, added quantity to Grocery

--- src/main/java/no/ntnu/idatt1005/Grocery/Grocery.java
+++ src/main/java/no/ntnu/idatt1005/Grocery/Grocery.java
@@ -7,14 +7,16 @@ public class Grocery {
     private final String name;
     private final String image;
     private final String id;
+    private final int quantity;
     private final UnitsE unit;
 
 
-    public Grocery(String name, String image, String id, UnitsE unit) {
+    public Grocery(String name, String image, String id, UnitsE unit, int quantity) {
         this.name = name;
         this.image = image;
         this.id = id;
         this.unit = unit;
+        this.quantity = quantity;
     }
 
     public String getName() {
@@ -30,6 +32,10 @@ public class Grocery {
 
     public UnitsE getUnit() {
         return unit;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
 
--- src/main/java/no/ntnu/idatt1005/view/FridgeTab.java
+++ src/main/java/no/ntnu/idatt1005/view/FridgeTab.java
@@ -9,6 +9,9 @@ import no.ntnu.idatt1005.Grocery.Grocery;
 import no.ntnu.idatt1005.Grocery.GroceryList;
 import no.ntnu.idatt1005.Unit.UnitsE;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 public class FridgeTab extends SuperTab {
@@ -24,26 +27,33 @@ public class FridgeTab extends SuperTab {
     }
 
     private void initializeTestData() {
-        groceryList.addGrocery(new Grocery("Melk", "melk.png", "001", UnitsE.VOLUME));
-        groceryList.addGrocery(new Grocery("Brød", "brod.png", "002", UnitsE.ARTICLE));
-        groceryList.addGrocery(new Grocery("Ost", "ost.png", "003", UnitsE.WEIGHT));
-        groceryList.addGrocery(new Grocery("Egg", "egg.png", "004", UnitsE.ARTICLE));
+        groceryList.addGrocery(new Grocery("Melk", "melk.png", "001", UnitsE.VOLUME, 1000));
+        groceryList.addGrocery(new Grocery("Brød", "brod.png", "002", UnitsE.ARTICLE, 2));
+        groceryList.addGrocery(new Grocery("Ost", "ost.png", "003", UnitsE.WEIGHT, 500));
+        groceryList.addGrocery(new Grocery("Egg", "egg.png", "004", UnitsE.ARTICLE, 6));
     }
 
     private void initializeUI() {
         VBox contentBox = new VBox(10);
         Button addButton = new Button("Legg til Grocery");
         addButton.setOnAction(e -> addGrocery());
-
     
         Button removeButton = new Button("Fjern Grocery");
         removeButton.setOnAction(e -> removeGrocery());
     
-        contentBox.getChildren().addAll(addButton, removeButton, groceryItemsBox);
-        refreshGroceryItemsBox();
-        this.setContent(contentBox);
-    }
+        contentBox.getChildren().addAll(addButton, removeButton);
 
+        ScrollPane scrollPane = new ScrollPane(); 
+        scrollPane.setContent(groceryItemsBox); 
+        scrollPane.setPrefViewportHeight(800); 
+    
+        contentBox.getChildren().add(scrollPane);
+    
+        refreshGroceryItemsBox(); 
+        this.setContent(contentBox); 
+    }
+    
+    
     private void addGrocery() {
         TextInputDialog nameDialog = new TextInputDialog();
         nameDialog.setTitle("Ny Vare");
@@ -52,15 +62,22 @@ public class FridgeTab extends SuperTab {
         Optional<String> nameResult = nameDialog.showAndWait();
         if (!nameResult.isPresent()) return;
     
+        String id;
+    while (true) {
         TextInputDialog idDialog = new TextInputDialog();
         idDialog.setTitle("Ny Vare ID");
         idDialog.setHeaderText("Legg til en ny vare ID");
         idDialog.setContentText("Vennligst oppgi ID:");
         Optional<String> idResult = idDialog.showAndWait();
-        if (!idResult.isPresent() || groceryList.getGroceryList().containsKey(idResult.get())) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "ID er allerede tatt eller ikke oppgitt.");
+        if (!idResult.isPresent()) return;
+
+        id = idResult.get();
+        if (!id.matches("\\d+") || groceryList.getGroceryList().containsKey(id)) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "ID er allerede tatt, ikke oppgitt, eller ikke et tall.");
             alert.showAndWait();
-            return; 
+        } else {
+            break;
+        }
         }
     
         ChoiceDialog<UnitsE> unitDialog = new ChoiceDialog<>(UnitsE.ARTICLE, UnitsE.values());
@@ -70,28 +87,50 @@ public class FridgeTab extends SuperTab {
         Optional<UnitsE> unitResult = unitDialog.showAndWait();
         if (!unitResult.isPresent()) return;
     
-        Grocery newGrocery = new Grocery(nameResult.get(), "", idResult.get(), unitResult.get());
+        String quantity;
+        while (true) {
+        TextInputDialog quantityDialog = new TextInputDialog();
+        quantityDialog.setTitle("Ny Vare Mengde");
+        quantityDialog.setHeaderText("Legg til en ny vare Mengde");
+        quantityDialog.setContentText("Vennligst oppgi mengde:");
+        Optional<String> quantityResult = quantityDialog.showAndWait();
+        if (!quantityResult.isPresent()) return;
+
+        quantity = quantityResult.get();
+        if (!quantity.matches("\\d+")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Mengde må være et tall.");
+            alert.showAndWait();
+        } else {
+            break;
+        }
+        }
+    
+        Grocery newGrocery = new Grocery(nameResult.get(), "", id, unitResult.get(), Integer.parseInt(quantity));
         groceryList.addGrocery(newGrocery);
-        refreshGroceryItemsBox();
+        Platform.runLater(() -> refreshGroceryItemsBox());
     }
     
 
     private Label createGroceryItem(Grocery grocery) {
-        Label nameLabel = new Label(grocery.getName() + "  " + grocery.getUnit().getUnit() + " -  id: " + grocery.getId());
+        Label nameLabel = new Label(grocery.getName() + "  " + grocery.getQuantity()+ " " + grocery.getUnit().getUnit() + " -  id: " + grocery.getId());
         nameLabel.setFont(Font.font("Arial", FontWeight.BOLD, 18));
         return nameLabel;
     }
     
 
     private void refreshGroceryItemsBox() {
-        Platform.runLater(() -> {
-            groceryItemsBox.getChildren().clear(); 
-            for (Grocery grocery : groceryList.getGroceryList().values()) {
-                Label groceryLabel = createGroceryItem(grocery); 
-                groceryItemsBox.getChildren().add(groceryLabel); 
-            }
-        });
-    }
+    Platform.runLater(() -> {
+        groceryItemsBox.getChildren().clear(); 
+        List<Grocery> sortedGroceries = new ArrayList<>(groceryList.getGroceryList().values());
+        sortedGroceries.sort(Comparator.comparing(Grocery::getId));
+
+        for (Grocery grocery : sortedGroceries) {
+            Label groceryLabel = createGroceryItem(grocery); 
+            groceryItemsBox.getChildren().add(groceryLabel); 
+        }
+    });
+}
+
 
     private void removeGrocery() {
         TextInputDialog idDialog = new TextInputDialog();
