b6229f479bafd294d04dd9154fbf9038443b0535
Jacob Lein <jacoble@stud.ntnu.no>
2024-03-26 16:18:28 +0100

    Create shoppingCartTab with all recipes and buttons

--- src/main/java/no/ntnu/idatt1005/view/ShoppingCartTab.java
+++ src/main/java/no/ntnu/idatt1005/view/ShoppingCartTab.java
@@ -1,33 +1,143 @@
 package no.ntnu.idatt1005.view;
 
-import javafx.scene.control.Tab;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import no.ntnu.idatt1005.Recipe.Recipe;
 import no.ntnu.idatt1005.controller.BasketController;
+import no.ntnu.idatt1005.controller.RecipeController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 //This class is used to create and update the shopping cart tab
 public class ShoppingCartTab extends SuperTab {
     private final Text shoppingCartTitle = new Text("Shopping Cart");
+    private final BasketController basketController = new BasketController();
+    private final RecipeController recipeController = new RecipeController();
+    private final HBox recipeBox = allRecipes();
+    private Map<String,Integer> recipeAmountMap;
+
     public ShoppingCartTab() {
         super("Shopping Cart");
     }
-    public void defaultTabCreation() {
+    public VBox defaultTabCreation() {
 
         this.setClosable(false);
 
         VBox shoppingCartContent = new VBox();
+        shoppingCartContent.setSpacing(10);
+
+        //Kan vurdere å legge til i superklassen
+        shoppingCartContent.setPadding(new Insets(30));
         Text shoppingCartDescription = new Text("Please click on the desired recipes to add it to your shopping cart. The required goods to buy will be calculated later.");
         shoppingCartDescription.setFont(this.getDescriptionFont());
 
         shoppingCartContent.getChildren().addAll(shoppingCartTitle, shoppingCartDescription);
         shoppingCartTitle.setFont(this.getTitleFont());
-
-        BasketController basketController = new BasketController();
         basketController.getShoppingListFromBasket();
 
 
-        this.setContent(shoppingCartContent);
+        return shoppingCartContent;
     }
+
+    public HBox allRecipes(){
+
+        HBox recipeBox = new HBox();
+        recipeBox.setSpacing(10);
+        recipeBox.setPadding(new Insets(30));
+        List<Recipe> recipeList = recipeController.getAllRecipes();
+
+        for (int i = 0; i < recipeList.size(); i++) {
+            //Create a recipe box with text and counter
+            VBox currentRecipeBox = new VBox();
+            Recipe recipe = recipeList.get(i);
+
+            //Get recipe name, set font and add to the currentRecipeBox
+            Text recipeName = new Text(recipe.getRecipeName());
+            recipeName.setFont(this.getUnderTitleFont());
+
+            Text counterText = new Text("0");
+            Button decreaseButton = new Button("-");
+            Button increaseButton = new Button("+");
+
+            //Add responses to the buttons
+            decreaseButton.setOnAction(e -> {
+                int currentAmount = Integer.parseInt(counterText.getText());
+                if (currentAmount > 0) {
+                    counterText.setText(Integer.toString(currentAmount - 1));
+                }
+            });
+
+            increaseButton.setOnAction(e -> {
+                int currentAmount = Integer.parseInt(counterText.getText());
+                counterText.setText(Integer.toString(currentAmount + 1));
+            });
+
+            HBox counterBox = new HBox(decreaseButton, counterText, increaseButton);
+            counterBox.setSpacing(10);
+            currentRecipeBox.getChildren().addAll(recipeName,counterBox);
+            recipeBox.getChildren().add(currentRecipeBox);
+        }
+        return recipeBox;
+    }
+
+    public Button createFinishShoppingButton(){
+        Button finishShoppingButton = new Button("Finish shopping ->");
+        finishShoppingButton.setOnAction(e -> {
+            recipeAmountMap = new HashMap<>();
+
+            for (int i = 0; i < recipeBox.getChildren().size(); i++) {
+                if (recipeBox.getChildren().get(i) instanceof VBox){
+                    VBox currentRecipeBox = (VBox) recipeBox.getChildren().get(i);
+
+                    //Finds each recipe box and gets the recipe name and the amount of the recipe
+                    Node recipeNameNode = currentRecipeBox.getChildren().get(0);
+                    Node counterBoxNode = currentRecipeBox.getChildren().get(1);
+
+                    String recipeName = ((Text) recipeNameNode).getText();
+                    //Gets the amount of the recipe
+                    String amount = ((Text) ((HBox) counterBoxNode).getChildren().get(1)).getText();
+                    recipeAmountMap.put(recipeName, Integer.parseInt(amount));
+                }
+            }
+
+            StackPane finishShoppingTempPane = createFinishShoppingTempPane();
+
+
+            this.setContent(finishShoppingTempPane);
+        });
+
+        return finishShoppingButton;
+    }
+
+    public StackPane createFinishShoppingTempPane(){
+        StackPane finishShoppingTempPane = new StackPane();
+
+        VBox finishShoppingContent = new VBox();
+        Text shoppingListText = new Text("Shopping List:");
+        finishShoppingContent.getChildren().addAll(shoppingCartTitle, shoppingListText);
+
+
+        recipeAmountMap.forEach((recipeName, amount) -> {
+            Text recipeText = new Text(recipeName + ": " + amount);
+            finishShoppingContent.getChildren().add(recipeText);
+        });
+
+        Text counterTextTest1 = new Text();
+        finishShoppingTempPane.getChildren().add(finishShoppingContent);
+        return finishShoppingTempPane;
+    }
+
+    public Map<String, Integer> getRecipeAmountMap() {
+        return recipeAmountMap;
+    }
+
 
     /*
     // Create a recipe box with text and counter
@@ -59,17 +169,11 @@ public class ShoppingCartTab extends SuperTab {
             //Create HBox for the result button
             HBox resultButtonBox = new HBox();
             resultButtonBox.setAlignment(Pos.BOTTOM_RIGHT);
-            Button resultButton = new Button("Finish shopping ->");
-            resultButtonBox.getChildren().add(resultButton);
-            shoppingCartContent.getChildren().add(resultButtonBox);
+
 
             //Node originalContent = tabShoppingCart.getContent();
             //Create a new scene for the shopping cart
-            resultButton.setOnAction(e ->{
-                tabShoppingCart.setContent(null);
-                Scene shoppingCartScene = new Scene(tabPane);
-                Text shoppingListText = new Text("Shopping List");
-                shoppingCartContent.getChildren().addAll(shoppingCartText, shoppingListText);
+
 
 
                 BasketController basketController = new BasketController();
