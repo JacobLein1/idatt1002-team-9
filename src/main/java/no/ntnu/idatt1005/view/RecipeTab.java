201c209273f18398147787530e6045eb0a5eee9e
Jacob Lein <jacoble@stud.ntnu.no>
2024-04-05 12:56:38 +0200

    Commit before enum change pull

--- src/main/java/no/ntnu/idatt1005/view/RecipeTab.java
+++ src/main/java/no/ntnu/idatt1005/view/RecipeTab.java
@@ -9,7 +9,10 @@ import no.ntnu.idatt1005.RecipeInfo.Ingredient;
 import no.ntnu.idatt1005.RecipeInfo.Recipe;
 import no.ntnu.idatt1005.controller.RecipeController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class RecipeTab extends SuperTab {
     RecipeController recipeController = new RecipeController();
@@ -32,13 +35,21 @@ public RecipeTab() {
 
         recipeContent.setSpacing(10);
         recipeContent.getChildren().add(recipeUnderTitle);
-
+        recipeContent.getChildren().add(recipesBox());
         return recipeContent;
     }
 
     public HBox recipesBox() {
         HBox recipeBox = new HBox();
         recipeBox.setSpacing(10);
+
+        List<Recipe> recipes = recipeController.getAllRecipes();
+        System.out.println(recipes.size());
+        for (Recipe recipe : recipes) {
+            VBox singleRecipeBox = singleRecipeBox(recipe);
+            recipeBox.getChildren().add(singleRecipeBox);
+        }
+
         return recipeBox;
     }
 
@@ -51,14 +62,21 @@ public RecipeTab() {
         //Add the list of ingredients to the single recipe box
         Text recipeIngredientsIntro = new Text("Ingredients:");
         List<Ingredient> listOfIngredients = recipeController.getListOfIngredientForRecipe(recipe.getRecipeID());
+
         VBox ingredientBox = new VBox();
         ingredientBox.setSpacing(10);
-        listOfIngredients.stream().forEach(ingredient -> {
+
+        HashMap<String,String> groceryList = recipeController.getIngredientsForRecipe(recipe.getRecipeID());
+
+        for (Map.Entry<String, String> entry : groceryList.entrySet()) {
+            String key = entry.getKey(); // Ingredient name
+            String value = entry.getValue(); // Ingredient amount and unit
+
+            //Add each ingredient to ingredientBox
+            ingredientBox.getChildren().add(new Text(key + ": " + value));
+        }
 
 
-            //ingredientBox.getChildren().add(ingredient.get);
-
-        });
         singleRecipeBox.getChildren().addAll(recipeTitle,recipeIngredientsIntro,ingredientBox);
 
         Text recipeInstructionsIntro = new Text("Instructions:");
